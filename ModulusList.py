56ffff32e7b148beb4ebe07556beb517f00cd48b
MrBhendel <bthendel@gmail.com>
2019-02-17 14:41:02 -0600

    Made final changes and added unit tests for ModulusList.py

--- ModulusList.py
+++ ModulusList.py
@@ -2,34 +2,35 @@ import sys
 
 class ModulusList():
 
+	'''
+	Maintains a list of (host, modulus, e) tuples.
+	'''
+
 	def __init__(self):
-		self.modulusList = []
+		self._modulusList = []
 
-	def extendList(self, newList):
-		self.modulusList.extend(newList)
+	def add(self, host, modulus, e):
+		self._modulusList.append((host, modulus, e))
 
-	def addToList(self, item):
-		self.modulusList.append(item)
+	def length(self):
+		return len(self._modulusList)
 
-	def getListLength(self):
-		return len(self.modulusList)
-
-	def getListElement(self, index):
-		return self.modulusList[index]
+	def __getitem__(self, index):
+		return self._modulusList[index]
 
 	def saveListToFile(self, fileName):
 		saveFile = open(fileName, 'w')
-		for record in self.modulusList:
+		for record in self._modulusList:
 			saveFile.write(str(record[0]) + '\n')
 			saveFile.write(str(record[1]) + '\n')
 			saveFile.write(str(record[2]) + '\n')
 		saveFile.close()
 
-	def loadKeys(self, fileName):
+	def loadListFromFile(self, fileName):
 		loadFile = open(fileName, 'r')
 		while True:
 			host = loadFile.readline().rstrip()
 			n = loadFile.readline().rstrip()
 			e = loadFile.readline().rstrip()
 			if not e: break
-			self.modulusList.append((host, long(n), long(e)))
+			self._modulusList.append((host, long(n), long(e)))
